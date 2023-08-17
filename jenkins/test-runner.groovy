2350e3bd79d37e8cb2069eaef353390f5b516ddf
Nadiia Vorobiova <nadiia.vorobiova@refinitiv.com>
2023-08-17 10:31:54 +0200

    fix test-runner

--- jenkins/test-runner.groovy
+++ jenkins/test-runner.groovy
@@ -7,7 +7,7 @@ timeout(5) {
             def jobs = [:]
 
             jobs['ui-tests'] = {
-                node('maven-slave') {
+                node('maven') {
                     stage('Ui tests on chrome') {
                         build(job: 'ui-tests',
                                 parameters: [
@@ -20,24 +20,22 @@ timeout(5) {
                     }
                 }
             }
-        }
-
-        jobs['api-tests'] = {
-            node('maven') {
-                stage('API tests on chrome') {
-                    build(job: 'api-tests',
-                            parameters: [
-                                    string(name: 'BRANCH', value: BROWSER_NAME),
-                                    string(name: 'BASE_URL', value: BASE_URL),
-                                    string(name: 'BROWSER', value: BROWSER_NAME),
-                                    string(name: 'BROWSER_VERSION', value: BROWSER_VERSION),
-                                    string(name: 'GRID_URL', value: GRID_URL)
-                            ])
+            jobs['api-tests'] = {
+                node('maven') {
+                    stage('API tests on chrome') {
+                        build(job: 'api-tests',
+                                parameters: [
+                                        string(name: 'BRANCH', value: BROWSER_NAME),
+                                        string(name: 'BASE_URL', value: BASE_URL),
+                                        string(name: 'BROWSER', value: BROWSER_NAME),
+                                        string(name: 'BROWSER_VERSION', value: BROWSER_VERSION),
+                                        string(name: 'GRID_URL', value: GRID_URL)
+                                ])
+                    }
                 }
             }
+            parallel jobs
         }
-        parallel jobs
-
         stage('Publish API artifacts') {
             allure([results          : [[
                                                 path: 'allure-results'
