98e40795a2d8e924ef1e4ea0dc483aea519c69c0
Nadiia Vorobiova <nadiia.vorobiova@refinitiv.com>
2023-08-17 10:18:24 +0200

    added nre test-runner.groovy

--- jenkins/test-runner.groovy
+++ jenkins/test-runner.groovy
@@ -1,36 +1,65 @@
-pipeline {
-    agent any
-    stages {
-        stage('MultiJob like stage') {
-            parallel {
-                stage('Run UI tests') {
-                    build(job: 'ui-tests',
-                            parameters: [
-                                    string(name: 'BRANCH', value: BRANCH),
-                                    string(name: 'BASE_URL', value: BASE_URL),
-                                    string(name: 'BROWSER', value: BROWSER),
-                                    string(name: 'BROWSER_VERSION', value: BROWSER_VERSION),
-                                    string(name: 'GRID_URL', value: GRID_URL)
-                            ])
-                }
-                stage('Run API tests') {
-                    build(job: 'api-tests',
-                            parameters: [
-                                    string(name: 'BRANCH', value: BRANCH),
-                                    string(name: 'BASE_URL', value: BASE_URL),
-                                    string(name: 'BROWSER', value: BROWSER),
-                                    string(name: 'BROWSER_VERSION', value: BROWSER_VERSION),
-                                    string(name: 'GRID_URL', value: GRID_URL)
-                            ])
+timeout(5) {
+    node('maven') {
+        stage('Checkout') {
+            checkout scm
+        }
+        stage('Run tests') {
+            def jobs = [:]
+
+            def runnerJobs = "$TEST_TYPE".split(",")
+
+            jobs['ui-tests'] = {
+                node('maven-slave') {
+                    stage('Ui tests on chrome') {
+                        if('ui' in runnerJobs) {
+                            catchError(buildResult: 'SUCCESS', stageResult: 'UNSTABLE') {
+                                build(job: 'ui-tests',
+                                        parameters: [
+                                                string(name: 'BRANCH', value: BROWSER_NAME),
+                                                string(name: 'BASE_URL', value: BASE_URL),
+                                                string(name: 'BROWSER', value: BROWSER_NAME),
+                                                string(name: 'BROWSER_VERSION', value: BROWSER_VERSION),
+                                                string(name: 'GRID_URL', value: GRID_URL)
+                                        ])
+                            }
+                        } else {
+                            echo 'Skipping stage...'
+                            Utils.markStageSkippedForConditional('keystone ui tests')
+                        }
+                    }
                 }
             }
-            stage('Publish API artifacts') {
-                allure([results          : [[
-                                                    path: 'allure-results'
-                                            ]],
-                        disabled         : false,
-                        reportBuildPolicy: 'ALWAYS'])
+
+            jobs['api-tests'] = {
+                node('maven-slave') {
+                    stage('API tests on chrome') {
+                        if('api' in runnerJobs) {
+                            catchError(buildResult: 'SUCCESS', stageResult: 'UNSTABLE') {
+                                build(job: 'api-tests',
+                                        parameters: [
+                                                string(name: 'BRANCH', value: BROWSER_NAME),
+                                                string(name: 'BASE_URL', value: BASE_URL),
+                                                string(name: 'BROWSER', value: BROWSER_NAME),
+                                                string(name: 'BROWSER_VERSION', value: BROWSER_VERSION),
+                                                string(name: 'GRID_URL', value: GRID_URL)
+                                        ])
+                            }
+                        } else {
+                            echo 'Skipping stage...'
+                            Utils.markStageSkippedForConditional('keystone api tests')
+                        }
+                    }
+                }
             }
+            parallel jobs
+        }
+
+        stage('Publish API artifacts') {
+            allure([results          : [[
+                                                path: 'allure-results'
+                                        ]],
+                    disabled         : false,
+                    reportBuildPolicy: 'ALWAYS'])
         }
     }
 }
