e65a4825b11958c4be4dcd8c5d7d8605e226a321
Nadiia Vorobiova <nadiia.vorobiova@refinitiv.com>
2023-08-17 11:47:58 +0200

    rm stages step

--- jenkins/test-runner.groovy
+++ jenkins/test-runner.groovy
@@ -1,42 +1,40 @@
 timeout(5) {
     node('maven') {
-        stages {
-            stage('Checkout') {
-                checkout scm
-            }
-            stage('Run Tests') {
-                parallel {
-                    stage('Run UI tests') {
-                        def exitCode = sh(
-                                returnStatus: true,
-                                script: """
+        stage('Checkout') {
+            checkout scm
+        }
+        stage('Run Tests') {
+            parallel {
+                stage('Run UI tests') {
+                    def exitCode = sh(
+                            returnStatus: true,
+                            script: """
                     mvn test -Dbrowser=$BROWSER_NAME -Dbrowser.version=$BROWSER_VERSION -Dwebdriver.base.url=$BASE_URL -Dwebdriver.remote.url=$GRID_URL
                     """
-                        )
-                        if (exitCode != 0) {
-                            currentBuild.result = 'UNSTABLE'
-                        }
+                    )
+                    if (exitCode != 0) {
+                        currentBuild.result = 'UNSTABLE'
                     }
-                    stage('Run API tests') {
-                        def exitCode = sh(
-                                returnStatus: true,
-                                script: """
+                }
+                stage('Run API tests') {
+                    def exitCode = sh(
+                            returnStatus: true,
+                            script: """
                     mvn test -Dbrowser=$BROWSER_NAME -Dbrowser.version=$BROWSER_VERSION -Dwebdriver.base.url=$BASE_URL -Dwebdriver.remote.url=$GRID_URL
                     """
-                        )
-                        if (exitCode != 0) {
-                            currentBuild.result = 'UNSTABLE'
-                        }
+                    )
+                    if (exitCode != 0) {
+                        currentBuild.result = 'UNSTABLE'
                     }
                 }
             }
-            stage('Publish artifacts') {
-                allure([results          : [[
-                                                    path: 'allure-results'
-                                            ]],
-                        disabled         : false,
-                        reportBuildPolicy: 'ALWAYS'])
-            }
+        }
+        stage('Publish artifacts') {
+            allure([results          : [[
+                                                path: 'allure-results'
+                                        ]],
+                    disabled         : false,
+                    reportBuildPolicy: 'ALWAYS'])
         }
     }
 }
